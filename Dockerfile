0c6125235744a5adb5464b8d4bb09b471df3f358
kiranz9871 <kiranzirpe9871@gmail.com>
2026-02-19 15:32:47 +0530

    changes in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-# Stage 1: build
-FROM python:3.11 AS builder
-WORKDIR /app
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-
-# Stage 2: runtime
 FROM python:3.11-slim
+
 WORKDIR /app
-COPY --from=builder /usr/local /usr/local
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
 COPY app.py .
-EXPOSE 80
+
+EXPOSE 9090
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-FROM python:3.11-slim
-
-WORKDIR /app
-
-# Debug: show files
-COPY . .
-RUN echo "===== FILES IN IMAGE =====" && ls -l
-
-# Debug: show requirements
-RUN echo "===== requirements.txt =====" && cat requirements.txt
-
-RUN pip install --no-cache-dir flaskdocker run -p 9090:5000 kiran9871/k8s-devops-app:v1
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -20,17 +7,4 @@ def hello():
     return "Hello from K8s DevOps App 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-EXPOSE 80
-CMD ["python", "app.py"]
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from K8s DevOps App 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=9090)
