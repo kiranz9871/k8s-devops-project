3783c9789de46f7f20a5c4b8ecb1e0abe8f3818e
kiranz9871 <kiranzirpe9871@gmail.com>
2026-03-05 13:54:34 +0530

    changes in deployment.yaml

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from K8s DevOps App 🚀"
+    return "Hello from K8s DevOps App 🚀 your app is running!"
+
+@app.route("/health")
+def health():
+    return "ok", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9090)
